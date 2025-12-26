e39336036b03f77164a071d40f3c1546f6cb2e5a
Valera <val12042025@gmail.com>
2025-12-26 11:57:59 +0300

    finish

--- init_dgraph.sh
+++ init_dgraph.sh
@@ -2,18 +2,8 @@
 set -e
 
 # 1. Очистка
-sudo rm -rf dgraph/{zero1,zero2,zero3,alpha1,alpha2}
-mkdir -p dgraph/{zero1,zero2,zero3,alpha1,alpha2}
+sudo rm -rf dgraph/{zero1,zero2,zero3,alpha1,alpha2,alpha3}
+mkdir -p dgraph/{zero1,zero2,zero3,alpha1,alpha2,alpha3}
 
 # 2. Запуск Zero1
-docker-compose up -d zero1
-# Ждём 10–15 секунд, пока zero1 станет лидером
-sleep 30
-# 3. Запуск остальных Zero
-docker-compose up -d zero2 zero3
-# Ждём 10 секунд
-sleep 30
-# 4. Проверка: все Zero работают?
-
-# 5. Запуск Alpha
-docker-compose up -d alpha1 alpha2 alpha3 ratel
+docker-compose up -d 
